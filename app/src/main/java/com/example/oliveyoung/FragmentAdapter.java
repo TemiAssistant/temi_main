2eefea88c1af7391572c5a5507644942d40702a4
godcomet <comet20002@gmail.com>
2025-11-25 20:11:19 +0900

    [fix] 탭 3개 안띄워지는 현상 해결

--- app/src/main/java/com/example/oliveyoung/FragmentAdapter.java
+++ app/src/main/java/com/example/oliveyoung/FragmentAdapter.java
@@ -1,34 +1,33 @@
 package com.example.oliveyoung;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 public class FragmentAdapter extends FragmentStateAdapter {
 
-    public FragmentAdapter(@NonNull AppCompatActivity activity) {
-        super(activity);
+    public FragmentAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
     }
 
     @NonNull
     @Override
     public Fragment createFragment(int position) {
-        // 탭에 맞는 Fragment를 반환
         switch (position) {
             case 0:
-                return new FollowFragment();  // FollowFragment는 사용자가 정의한 Fragment
+                return new FollowFragment();
             case 1:
-                return new SearchFragment(); // SearchFragment는 사용자가 정의한 Fragment
+                return new SearchFragment();
             case 2:
-                return new CheckoutFragment(); // CheckoutFragment는 사용자가 정의한 Fragment
+                return new CheckoutFragment();
             default:
-                return new FollowFragment();  // 기본값
+                return new FollowFragment();
         }
     }
 
     @Override
     public int getItemCount() {
-        return 3; // 3개의 탭
+        return 3;
     }
 }
